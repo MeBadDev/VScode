6651acdbd86a102b59b36a95ffbcd41bf4e554c8
MeBadDev <94725241+MeBadDev@users.noreply.github.com>
2023-12-30 12:15:58 +0800

    Adaptive editor

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ const EDITOR_1 = document.getElementById("editor-1") as HTMLDivElement
 const EDITOR_2 = document.getElementById("editor-2") as HTMLDivElement
 
 monaco.editor.create(EDITOR_1, {
-  language: "typescript"
+  language: "javascript"
 })
 monaco.editor.create(EDITOR_2, {
-  language: "typescript"
+  language: "javascript"
 })
