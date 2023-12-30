f684f35fa50794d0fb677da1ed9940f851e8bd7e
MeBadDev <94725241+MeBadDev@users.noreply.github.com>
2023-12-30 12:06:53 +0800

    Implemented Monaco Editor

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-import './style.css'
+import * as monaco from 'monaco-editor'
+import './index.css'
+
+const EDITOR_1 = document.getElementById("editor-1") as HTMLDivElement
+const EDITOR_2 = document.getElementById("editor-2") as HTMLDivElement
+
+monaco.editor.create(EDITOR_1, {
+  language: "typescript"
+})
+monaco.editor.create(EDITOR_2, {
+  language: "typescript"
+})
